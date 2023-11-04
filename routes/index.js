5db431f44ff2733034b1e567449211901be65879
alejandro2207 <ale_gallegos.b@hotmail.com>
2023-11-04 12:58:26 -0600

    Primer commit y creacion de rama de trabajo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const productos = [
+  {
+    condigoProducto: '1209-K-TVH-C3',
+    descripcion: 'Morbi at turpis ac turpis pulvinar vehicula. Etiam sit amet justo nulla. Integer',
+    marca: 'FAG',
+    precio: 400,
+    disponibilidad: 22
+  },
+  {
+    condigoProducto: '1209-K-TVH-C3',
+    descripcion: 'Morbi at turpis ac turpis pulvinar vehicula. Etiam sit amet justo nulla. Integer',
+    marca: 'FAG',
+    precio: 400,
+    disponibilidad: 22
+  },
+  {
+    condigoProducto: '1209-K-TVH-C3',
+    descripcion: 'Morbi at turpis ac turpis pulvinar vehicula. Etiam sit amet justo nulla. Integer',
+    marca: 'FAG',
+    precio: 400,
+    disponibilidad: 22
+  },
+  {
+    condigoProducto: '1209-K-TVH-C3',
+    descripcion: 'Morbi at turpis ac turpis pulvinar vehicula. Etiam sit amet justo nulla. Integer',
+    marca: 'FAG',
+    precio: 400,
+    disponibilidad: 22
+  }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({saludos: "hola"})
+  res.send(productos)
+  //res.json({saludos: "hola"})
 });
 
 module.exports = router;
